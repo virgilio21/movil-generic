37b2c1598c3a7d82650d34ecdc32de5a0b8b52c0
pas24 <pascualgonzalez548@gmail.com>
2019-06-18 22:48:07 -0500

    Tercer commit

---
+++ app/src/main/java/com/genericsl/genericsl/MainActivity.java
@@ -0,0 +1,80 @@
+package com.genericsl.genericsl;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle togle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(togle);
+        togle.syncState();
+
+
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+           // navigationView.setCheckedItem(R.id.nav_producto);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        int item = menuItem.getItemId();
+
+        switch (item){
+            case R.id.nav_producto:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProductFragment()).commit();
+                break;
+
+            case R.id.nav_categoria:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new CategoryFragment()).commit();
+                break;
+
+            case R.id.nav_establecimiento:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new EstablishmentFragment()).commit();
+                break;
+
+            case R.id.nav_pedido:
+                Toast.makeText(this, "pedidos", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_help:
+                Toast.makeText(this, "ayuda", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/genericsl/genericsl/ProductFragment.java
@@ -0,0 +1,58 @@
+package com.genericsl.genericsl;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ProductFragment extends Fragment {
+
+
+    private Button btnSiguiente;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View fragment = inflater.inflate(R.layout.fragment_product,container,false);
+
+        final ArrayList<Source> lista = new ArrayList<Source>();
+        lista.add(new Source("Goku Super Saiyajin","hola",5.0f, 1.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 2","hola 2",50.0f, 2.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 3","hola 3",500.0f, 3.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 4","hola 4",5000.0f, 4.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 5","hola 5",4000.0f, 5.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 6","hola 6",400.0f, 6.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 7","hola 7",40.0f, 7.0f,R.drawable.p1));
+        lista.add(new Source("Goku Super Saiyajin 8","hola 8",4.0f, 8.0f,R.drawable.p1));
+
+        final RecyclerView contenedor = (RecyclerView) fragment.findViewById(R.id.contenedor);
+        contenedor.setHasFixedSize(true);
+        LinearLayoutManager layout = new LinearLayoutManager(fragment.getContext());
+        layout.setOrientation(LinearLayoutManager.VERTICAL);
+
+        Adapter adapter = new Adapter(lista);
+
+        adapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(fragment.getContext(),"Seleccionado el item: "+lista.get(contenedor.getChildAdapterPosition(v)).getCompany()+
+                                lista.get(contenedor.getChildAdapterPosition(v)).getPrice()+lista.get(contenedor.getChildAdapterPosition(v)).getRanking()
+                        ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        contenedor.setAdapter(adapter);
+        contenedor.setLayoutManager(layout);
+
+        return fragment;
+    }
+}
