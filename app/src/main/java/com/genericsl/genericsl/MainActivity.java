0a57b4a51bc044f8b248dc25d2711e91b6a13898
pas24 <pascualgonzalez548@gmail.com>
2019-08-01 19:38:48 -0500

    Cuarto Commit

--- app/src/main/java/com/genericsl/genericsl/MainActivity.java
+++ app/src/main/java/com/genericsl/genericsl/MainActivity.java
@@ -1,5 +1,6 @@
 package com.genericsl.genericsl;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -8,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -35,6 +38,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
            // navigationView.setCheckedItem(R.id.nav_producto);
         }
+
+
+        FloatingActionButton btnCar = (FloatingActionButton) findViewById(R.id.btnCarrito);
+        btnCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Si funciona",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 
@@ -66,13 +79,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.nav_pedido:
-                Toast.makeText(this, "pedidos", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new OrderFragment()).commit();
                 break;
 
+            case R.id.nav_configuracion:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ConfigurationFragment()).commit();
+                break;
+
+
             case R.id.nav_help:
-                Toast.makeText(this, "ayuda", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HelpFragment()).commit();
                 break;
         }
+
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
